822afdc34a54189633a06cb0325e86043bf49d3e
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-08-29 23:10:52 +0200

    Create SearchBar component class

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,1 +1,15 @@
-b
+import React from 'react';
+
+export class SearchBar extends React.Component {
+
+    render(){
+        return (
+            <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist" />
+            <button className="SearchButton">SEARCH</button>
+            </div>
+        );
+    }
+
+
+};
