a51333c9653015ea931c0ef505a1e0b0d764a41a
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-08-31 15:04:23 +0200

    Make playlist save button save an array of Spotify track URIs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,6 +10,7 @@ export default class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
     this.state = { 
       searchResults: [
         {name: 'song1', artist: 'artist1', album: 'album1', id: 1}, 
@@ -50,6 +51,10 @@ export default class App extends React.Component {
     this.setState({ playlistName: name })
   };
 
+  savePlaylist(){
+    let trackURIs = this.state.playlistTracks.map(track => {return track.uri });
+  };
+
   render(){
     return (
       <div>
@@ -58,7 +63,7 @@ export default class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
